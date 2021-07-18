bc065b87716f2d50265c31806e8fc1e582307feb
Russell Gordon <github@russellgordon.ca>
2021-07-18 11:36:50 -0400

    Initial Commit

---
+++ EnvironmentObjectBug/EnvironmentObjectBugApp.swift
@@ -0,0 +1,17 @@
+//
+//  EnvironmentObjectBugApp.swift
+//  EnvironmentObjectBug
+//
+//  Created by Russell Gordon on 2021-07-18.
+//
+
+import SwiftUI
+
+@main
+struct EnvironmentObjectBugApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
